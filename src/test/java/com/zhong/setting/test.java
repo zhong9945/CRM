4aa7d55597377deebc006326f48a17f6ae8b6cb1
Administrator <994599834@qq.com>
2021-05-30 14:47:31 +0800

    1.0

--- src/test/java/com/zhong/setting/test.java
+++ src/test/java/com/zhong/setting/test.java
@@ -4,14 +4,5 @@ import com.zhong.utils.DateTimeUtil;
 import com.zhong.utils.MD5Util;
 
 public class test {
-    public static void main(String[] args) {
-        String formatSystemTime = DateTimeUtil.getFormatSystemTime();
-        String time  = "2021-05-30 12:11:20";
-        int i = formatSystemTime.compareTo(time);
-        System.out.println(i);
 
-        MD5Util md = new MD5Util();
-        System.out.println(md.getMD5("guang994599834"));
-
-    }
 }
